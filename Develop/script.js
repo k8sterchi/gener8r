3bf77b0a9c3748ae01c8216b8431ab8385de27fc
k8 <katesterchi@gmail.com>
2023-07-04 08:45:41 -0500

    added acceptance criteria to the readme, began inspecting the functionality of the JavaScript

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,12 @@
+
 // Assignment code here
 
+function generatePassword(){
+return {
+  name: 'kate',
+
+}
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -7,11 +14,19 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password");  
+  
+  var inputConfigurations = document.querySelector('#input-configurations');
+ // inputConfigurations.setAttribute('style', 'display: block');
+  
+  //document.querySelector('#input-configurations').setAttribute('style', 'display: block');
 
-  passwordText.value = password;
+  passwordText.value = password.name;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// document.querySelector('#character-length').addEventListener('change',(event) => {
+//  document.querySelector('#length-result').innerText = event.target.value;
+//})
