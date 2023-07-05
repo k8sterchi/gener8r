41d19dc5eefcaf0e22fc139c1343a5e79d9ecfb5
k8 <katesterchi@gmail.com>
2023-07-05 08:10:03 -0500

    continued psuedo coding undefined text area when user doesnt select character type, practiced changing the button color using java script instead of css

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,7 @@
-
-//      ensure at least one character type is selected
-// 3. Generate password based on criteria.
-
-
 // Assignment code here
-// store data into variables i will need in order to write and generate password
-var characterLength = 8;
-// Empty array where user's choice will concat with to create pw criteria 
-var choiceArr = [];
+// store data into variables needed to write and generate password
+var characterLength = 8; //this value will get redefined based on user input
+var choiceArr = []; // Empty array where user's choice will concat with to create pw criteria 
 // Arrays to store all the characters
 var specialCharArr = ['_',')','(','*','&','^','%','$','#','@','!'];
 var lowerCaseArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
@@ -15,62 +9,58 @@ var upperCaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
 var numberArr = ['1','2','3','4','5','6','7','8','9','0'];
 
 // Get references to the #generate element
-// generateBtn holds the value of element in the html with the ID generate 
+// generateBtn holds the value of the ID generate tag in the html 
 var generateBtn = document.querySelector("#generate");
+document.querySelector('#generate').style.background = 'pink';
 
-// Add eventlistener to generateBtn runs the writePassword function after 'click' occurs
+// Add eventlistener to generateBtn; runs the writePassword function after 'click' occurs
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-  console.log('The generate button has been pushed; event listener working correctly.')
-// Ensure user selects at least one character type
+  console.log('The generate button has been pushed; event listener working correctly.');
+
   if (choiceArr.length <= 0) {
+    var undefinedTextArea = document.querySelector('#password');
     alert('you must select at least one character type');
+    console.log('The user has not selected a character type.');
+    undefinedTextArea.value = 'try again'; //not working... ???
     return;
     }
   var password = '';
-  // i stands for index, let index = 0 ...index referes to position within array...
-  // .floor to include 0 to access 0 index
-  // using the length of the choice array with math functions to generate a random number,
-  // using that random number
-  // try code here?
+  // length of choiceArr * number between 0 and 1 creates a random decimal, .floor rounds the decimal down, giving index position for one character of the pw
+  // loops according to criteria of the for loop until characterLength is satisfied 
   for (var i = 0; i < characterLength; i++){
   var randomIndex = Math.floor(Math.random() * choiceArr.length)
   password = password + choiceArr[randomIndex];
  }
-  if (!password){
-    return 'please try again';
-  } else { 
   return password;
 }
-}
 
-// writePassword has an undefined function generatePassword, 
-// for generatePassword to meet acceptance criteria will require a prompts function
+
 function writePassword () {
-  var correctPrompts = prompts(); // true or false
-  // passwordText is the area in which the pw text would appear
-  var passwordText = document.querySelector('#password');
+  var correctPrompts = userPrompts(); 
+  // changed passwordText to better describe what this variable is in the HTML (the text area)
+  var passwordTextArea = document.querySelector('#password');
 
   if (correctPrompts) {
     var newPassword = generatePassword();
-    // grabbing dom element and setting the value to the return value of generatePassword function
-    passwordText.value = newPassword;
+    // grabbing dom element and setting its value to the return value of generatePassword function
+    passwordTextArea.value = newPassword;
   } else {
-    passwordText.value = ''; // .value could also be .innerHTML
+    passwordTextArea.value = ''; 
   }
 
 }
-function prompts (){
+function userPrompts (){
   //set to empty so you always restart at empty. test if it's necessary
   choiceArr = [];
   //parseInt converts the returned string into an integer
-  characterLength = parseInt(prompt('Password Length: Choose between 8 and 128 characters'))
+  characterLength = parseInt(prompt('Password Length: Choose a number between 8 and 128.'))
   
   //make sure user has typed a number not a string, and between correct length 
   //if you type the right thing then this will return false
   if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
-    alert('Character length must be a number between 8 and 128.');
+    alert('Check that you are entering numerical digits between 8 and 128.');
     return false;
   } 
 
@@ -93,7 +83,7 @@ function prompts (){
 }
 
 // TODO: ensure that the user selects at least one character type:
-// if the user clicks cancel to all prompts, 
+// if the user clicks cancel to all userPrompts, 
 // then alert appears:'you must select at least one character type'
 // if choice array is empty, alert user to select at least one character type.
 // t or f array has length > 0
